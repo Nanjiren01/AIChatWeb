7748987f46505fa113a717ed1ac4fa3cd121300d
tanqicun-mac <411707420@qq.com>
2023-05-12 01:05:24 +0800

    AI Chat

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -120,7 +120,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>AI Chat</div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
         </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "AI Chat Web",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "AI Chat Web",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
