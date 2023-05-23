1e4a477c04d1099b444e7445b5d8dcaf6a9df842
tanqicun-mac <411707420@qq.com>
2023-05-23 12:08:11 +0800

    update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,21 +3,11 @@
 # Check if running on a supported system
 case "$(uname -s)" in
   Linux)
-    if [[ -f "/etc/lsb-release" ]]; then
-      . /etc/lsb-release
-      if [[ "$DISTRIB_ID" != "Ubuntu" ]]; then
-        echo "This script only works on Ubuntu, not $DISTRIB_ID."
-        exit 1
-      fi
-    else
-      if [[ ! "$(cat /etc/*-release | grep '^ID=')" =~ ^(ID=\"ubuntu\")|(ID=\"centos\")|(ID=\"arch\")$ ]]; then
-        echo "Unsupported Linux distribution."
-        exit 1
-      fi
-    fi
+    echo "Running on Linux"
     ;;
   Darwin)
-    echo "Running on MacOS."
+    echo "This script only works on Linux, not Mac OS."
+    exit 1
     ;;
   *)
     echo "Unsupported operating system."
@@ -25,41 +15,58 @@ case "$(uname -s)" in
     ;;
 esac
 
+# check whether installed docker & compose
+
 # Check if needed dependencies are installed and install if necessary
-if ! command -v node >/dev/null || ! command -v git >/dev/null || ! command -v yarn >/dev/null; then
+if ! command -v docker >/dev/null; then
   case "$(uname -s)" in
     Linux)
-      if [[ "$(cat /etc/*-release | grep '^ID=')" = "ID=ubuntu" ]]; then
-        sudo apt-get update
-        sudo apt-get -y install nodejs git yarn
-      elif [[ "$(cat /etc/*-release | grep '^ID=')" = "ID=centos" ]]; then
-        sudo yum -y install epel-release
-        sudo yum -y install nodejs git yarn
-      elif [[ "$(cat /etc/*-release | grep '^ID=')" = "ID=arch" ]]; then
-        sudo pacman -Syu -y
-        sudo pacman -S -y nodejs git yarn
-      else
-        echo "Unsupported Linux distribution"
-        exit 1
-      fi
+      echo "curl -o install-docker-v20.10.21.sh..."
+      curl -o install-docker-v20.10.21.sh https://releases.rancher.com/install-docker/20.10.21.sh
+      chmod +x install-docker-v20.10.21.sh
+      ./install-docker-v20.10.21.sh
+
+      # auto start on boot
+      systemctl enable docker
+      systemctl start docker
       ;;
     Darwin)
-      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-      brew install node git yarn
+      # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      # brew install node git yarn
       ;;
   esac
 fi
 
 # Clone the repository and install dependencies
-git clone https://github.com/Yidadaa/ChatGPT-Next-Web
-cd ChatGPT-Next-Web
-yarn install
+echo "curl -o docker-compose.yml..."
+curl -o docker-compose.yml https://raw.githubusercontent.com/Nanjiren01/AIChatWeb/main/docker-compose.yml
 
-# Prompt user for environment variables
-read -p "Enter OPENAI_API_KEY: " OPENAI_API_KEY
-read -p "Enter CODE: " CODE
-read -p "Enter PORT: " PORT
+echo "Please input the super admin username. "
+echo "Only letters and numbers are supported, the length should between 6 and 20, and they cannot start with a number."
+read -p "Username: " SUPER_USERNAME
+regex='^[A-Za-z][A-Za-z0-9]{5,19}$'
+if [[ $SUPER_USERNAME =~ $regex ]]; then
+    echo "Super Admin Username is valid."
+else
+    echo "Super Admin Username is invalid."
+    exit 1
+fi
 
-# Build and run the project using the environment variables
-OPENAI_API_KEY=$OPENAI_API_KEY CODE=$CODE PORT=$PORT yarn build
-OPENAI_API_KEY=$OPENAI_API_KEY CODE=$CODE PORT=$PORT yarn start
+echo "Please input the super admin password. "
+echo "Only letters and numbers are supported, and the length should between 6 and 20. "
+echo "You can change it on the web page after the Application running"
+read -p "Password: " SUPER_PASSWORD
+regex='^[A-Za-z0-9]{6,20}$'
+if [[ $SUPER_PASSWORD =~ $regex ]]; then
+    echo "Super Admin Password is valid."
+else
+    echo "Super Admin Password is invalid."
+    exit 1
+fi
+
+sed -i "s/SUPERADMIN_USERNAME:.*/SUPERADMIN_USERNAME: $SUPER_USERNAME/g" docker-compose.yml
+sed -i "s/SUPERADMIN_PASSWORD:.*/SUPERADMIN_PASSWORD: $SUPER_PASSWORD/g" docker-compose.yml
+
+
+docker compose up -d
+
