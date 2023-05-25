ba2b133bbdbbae181a4ea4e52e55a0c5e3eaaf97
tanqicun-mac <411707420@qq.com>
2023-05-26 01:57:29 +0800

    update upgrade.sh

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -2,5 +2,9 @@
 
 docker compose down
 
+# Clone the repository and install dependencies
+echo "update docker-compose.yml..."
+curl -o docker-compose.yml https://raw.githubusercontent.com/Nanjiren01/AIChatWeb/main/docker-compose.yml
+
 docker compose up -d
 
