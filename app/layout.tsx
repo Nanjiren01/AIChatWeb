92c9b991036ce1f6fd8ad5bc5f186985af1048d6
tanqicun-mac <411707420@qq.com>
2023-06-04 10:29:48 +0800

    comment out: fonts.proxy.ustclug.org

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,11 +35,11 @@ export default function RootLayout({
         />
         <meta name="version" content={buildConfig.commitId} />
         <link rel="manifest" href="/site.webmanifest"></link>
-        <link rel="preconnect" href="https://fonts.proxy.ustclug.org"></link>
+        {/* <link rel="preconnect" href="https://fonts.proxy.ustclug.org"></link>
         <link
           href="https://fonts.proxy.ustclug.org/css2?family=Noto+Sans+SC:wght@300;400;700;900&display=swap"
           rel="stylesheet"
-        ></link>
+        ></link> */}
         <script src="/serviceWorkerRegister.js" defer></script>
       </head>
       <body>{children}</body>
