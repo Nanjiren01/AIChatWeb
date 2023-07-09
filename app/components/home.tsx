2c5df715f7977e12596c6039d0b6d9452a1bebc8
tanqicun-mac <411707420@qq.com>
2023-07-09 11:13:03 +0800

    HashRouter -> BrowserRouter

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -19,7 +19,7 @@ import { ErrorBoundary } from "./error";
 import { getLang } from "../locales";
 
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Routes,
   Route,
   useLocation,
